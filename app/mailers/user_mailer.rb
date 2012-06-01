aa2acaf8b0ab416ad85a5ec7e87eb13a0e446656
Michael makarov <info1117@gmail.com>
2012-05-31 23:27:43 -0700

    send mail!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,9 +14,15 @@ class UserMailer < ActionMailer::Base
 
     mail( :from => "rails-school.heroku.com",
           :to => "info1117@gmail.com",
-          :subject => "New user has registered!",
+          :subject => "New user " + user.name.to_s + "has registered!",
           :text => "So user's name is " + user.name.to_s + "!"
     )
+    if user.subscribe
+      mail( :from => "rails-school.heroku.com",
+            :to => user.email,
+            :subject => "Thank you for registration on Rails School!",
+      )
+    end
 
   end
 
