161367877bc4fb662ba7e5701bb5ab3a4c8ab125
Michael Makarov <info1117@gmail.com>
2012-09-20 19:22:32 -0700

    using environmental variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -212,7 +212,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '442490832470487', '866e55e16132ba96d1872b6b240ee69d', {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+    config.omniauth :facebook, ENV['FACEBOOK_TOKEN'], ENV['FACEBOOK_SECRET'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
